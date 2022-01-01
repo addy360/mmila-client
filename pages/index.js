7537544208f4664974b2990e2c67e8a791ca8706
addy360 <abbyboy360@gmail.com>
2022-01-01 10:22:56 +0300

    mui setup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import { Backdrop, Button, CircularProgress } from "@mui/material";
+import { useState } from "react";
+
 const Index = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div>
-      <h4>hellow mila client version two</h4>
+      <Button onClick={() => setOpen(true)}>Show backdrop</Button>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={() => setOpen(false)}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </div>
   );
 };
